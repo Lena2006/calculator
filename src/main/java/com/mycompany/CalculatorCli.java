841f3bc17961740899848dce57fd4b1fbb315ee3
user <user@kab305-17>
2019-06-03 19:21:38 +0300

    Calculator2

--- src/main/java/com/mycompany/CalculatorCli.java
+++ src/main/java/com/mycompany/CalculatorCli.java
@@ -12,8 +12,9 @@ import java.util.function.DoubleConsumer;
 import java.util.stream.Stream;
 
 /**
+ * Является клиентским классом для вызова calculatorImp
  *
- * @author user
+ * @author Елена
  */
 public class CalculatorCli {
 
@@ -22,14 +23,34 @@ public class CalculatorCli {
     public CalculatorCli(Calkulator calculator) {
         this.calculator = calculator;
     }
-
+/**
+ * Запускает интерактивную сессию
+ * Считывает вводимые с консоли данные
+ * @param reader 
+ * имеет тип Reader
+ */
     public void runInteractiveSession(Reader reader) {
         runInteractiveSession(reader, System.out::println);
        // runInteractuveSession(reader, System.out::println);
      //   runInteractiveSession(reader);
 
     }
-
+/**
+ * Запускает интерактивную сессию
+ * Работает со Stream
+ * Раскладывает строчку разделенные точкой с запятой
+ *  @param reader 
+ * Abstract class for reading character streams.  The only methods that a
+ * subclass must implement are read(char[], int, int) and close().  Most
+ * subclasses, however, will override some of the methods defined here in order
+ * to provide higher efficiency, additional functionality, or both.
+ * @param resultConsumer 
+ * Represents an operation that accepts a single {@code double}-valued argument and
+ * returns no result.  This is the primitive type specialization of
+ * {@link Consumer} for {@code double}.  Unlike most other functional interfaces,
+ * {@code DoubleConsumer} is expected to operate via side-effects.
+ * 
+ */
     public void runInteractiveSession(Reader reader, DoubleConsumer resultConsumer) {
         new BufferedReader(reader).lines()
                 .flatMap(s -> Stream.of(s.split(";")))
--- src/main/java/com/mycompany/CalkulatorImp.java
+++ src/main/java/com/mycompany/CalkulatorImp.java
@@ -10,11 +10,18 @@ import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 /**
+ * Класс занимается вычислениями и передает полученное значение
  *
- * @author user
+ * @author Елена
  */
 public class CalkulatorImp implements Calkulator {
 
+    /**
+     * Метод вычисляет полученное значение
+     *
+     * @param expression тип строка,используется для вычислений
+     * @return тип строка ,возвращает вычсления
+     */
     @Override
     public double calculate(String expression) {
 //        return 0;
@@ -31,9 +38,35 @@ public class CalkulatorImp implements Calkulator {
         }
     }
 
+    /**
+     * Вычисление математических фунлций
+     *
+     * @param scriptEngine <code>ScriptEngine</code> is the fundamental
+     * interface whose methods must be fully functional in every implementation
+     * of this specification.
+     * <br><br>
+     * These methods provide basic scripting functionality. Applications written
+     * to this simple interface are expected to work with minimal modifications
+     * in every implementation. It includes methods that execute scripts, and
+     * ones that set and get values.
+     * <br><br>
+     * The values are key/value pairs of two types. The first type of pairs
+     * consists of those whose keys are reserved and defined in this
+     * specification or by individual implementations. The values in the pairs
+     * with reserved keys have specified meanings.
+     * <br><br>
+     * The other type of pairs consists of those that create Java language
+     * Bindings, the values are usually represented in scripts by the
+     * corresponding keys or by decorated forms of them.
+     * @throws ScriptException The generic <code>Exception</code> class for the
+     * Scripting APIs. Checked exception types thrown by underlying scripting
+     * implementations must be wrapped in instances of
+     * <code>ScriptException</code>. The class has members to store line and
+     * column numbers and filenames if this information is available.
+     */
     private static void defineMathFunctions(ScriptEngine scriptEngine) throws ScriptException {
-for (String function : new String[] {"sin", "cos", "sqrt"}) {
-    scriptEngine.eval("function " + function + "(x) { return Java.type('java.lang.Math')." + function +"(x); }");
-}
+        for (String function : new String[]{"sin", "cos", "sqrt"}) {
+            scriptEngine.eval("function " + function + "(x) { return Java.type('java.lang.Math')." + function + "(x); }");
+        }
     }
 }
